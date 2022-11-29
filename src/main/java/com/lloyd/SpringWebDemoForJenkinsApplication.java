fac69ef0f8f25e98296ee85149d493afae13ff95
melaniel68 <catherinelopez123@hotmail.co.uk>
2022-11-29 06:30:29 +0000

    Initial Commit

---
+++ src/main/java/com/lloyd/SpringWebDemoForJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.lloyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebDemoForJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebDemoForJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lloyd/SpringWebDemoForJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lloyd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebDemoForJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
